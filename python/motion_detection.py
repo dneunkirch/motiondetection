864879c4f38ba29718973ab54f4317fdc28f62e5
David Neunkirch <dneunkirch@communicode.de>
2017-03-02 22:18:00 +0100

    .

--- python/motion_detection.py
+++ python/motion_detection.py
@@ -65,6 +65,7 @@ class StreamingHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.send_header('WWW-Authenticate', 'Basic realm="Test"')
             return
 
+        self.protocol_version = 'HTTP/1.1'
         self.send_response(303)
         self.send_header('Location', '/blacklist.html')
         self.end_headers()
